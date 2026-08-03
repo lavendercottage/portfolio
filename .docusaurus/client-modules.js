7928a29c1cf5beb75de3b0c9daffc4dac444f00d
lavendercottage <annika.m.adams@gmail.com>
2026-08-03 21:26:15 +0000

    Ignore generated Docusaurus files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/workspaces/portfolio/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/portfolio/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/portfolio/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/portfolio/src/css/custom.css"),
-];
